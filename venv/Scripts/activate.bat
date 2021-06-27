b5cc7cc268d30596765ef7746d88322cd4e81e64
Faun <futa.faun@gmail.com>
2021-06-27 17:45:47 +0300

    Changed virtual env

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\Projects\FP_image_formatter\venv"
+set "VIRTUAL_ENV=D:\Projects\FP_gallery_formatter\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
